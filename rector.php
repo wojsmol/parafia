0b05602f9c10c81f07219939d799595b7f5c834f
wojsmol <wojsmol@wp.pl>
2025-10-22 01:22:08 +0200

    poprawa konfiguracji rectora

--- rector.php
+++ rector.php
@@ -3,11 +3,36 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/library',
-    ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0);
+return static function (RectorConfig $rectorConfig): void {
+    // Określamy ścieżki do plików, które mają być analizowane i refaktoryzowane
+    // Ścieżka ustawiona na __DIR__ (katalog główny repozytorium).
+    $rectorConfig->paths([
+        __DIR__,
+    ]);
+
+    // Wykluczamy katalogi
+    $rectorConfig->skip([
+        __DIR__ . '/vendor',
+        __DIR__ . '/rector.php',
+    ]);
+
+    // Ustawiamy docelową wersję PHP, do której ma migrować Rector
+    $rectorConfig->phpVersion(80400); // PHP 8.4
+
+    // Importujemy zestawy reguł
+    $rectorConfig->sets([
+        // Zestawy reguł do migracji na PHP 8.4 (najważniejsze)
+        SetList::PHP_84,
+        SetList::PHP_83,
+        SetList::PHP_82,
+        SetList::PHP_81,
+        SetList::PHP_80,
+
+        // Zestawy reguł modernizujących kod
+        SetList::CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
+        SetList::EARLY_RETURN,
+    ]);
+};
