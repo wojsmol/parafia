a24908801a3eb49bcae809c0534a272c86f801ad
wojsmol <wojsmol@wp.pl>
2025-10-22 02:01:19 +0200

    kolejna wersja konfiguracji rectora

--- rector.php
+++ rector.php
@@ -6,7 +6,6 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    // Określamy ścieżki do plików, które mają być analizowane i refaktoryzowane
     // Ścieżka ustawiona na __DIR__ (katalog główny repozytorium).
     $rectorConfig->paths([
         __DIR__,
@@ -18,21 +17,24 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/rector.php',
     ]);
 
-    // Ustawiamy docelową wersję PHP, do której ma migrować Rector
+    // Ustawiamy docelową wersję PHP
     $rectorConfig->phpVersion(80400); // PHP 8.4
 
     // Importujemy zestawy reguł
     $rectorConfig->sets([
-        // Zestawy reguł do migracji na PHP 8.4 (najważniejsze)
+        // Najważniejsze zestawy do migracji na PHP 8.4
         SetList::PHP_84,
         SetList::PHP_83,
         SetList::PHP_82,
         SetList::PHP_81,
         SetList::PHP_80,
 
-        // Zestawy reguł modernizujących kod
+        // Agresywne zestawy modernizujące:
         SetList::CODE_QUALITY,
-        SetList::TYPE_DECLARATION,
+        SetList::TYPE_DECLARATION, // Dodaje typy zwracane i właściwości
         SetList::EARLY_RETURN,
+        SetList::DEAD_CODE,         // Usuwa nieużywany kod i zmienne
+        SetList::PRIVATIZATION,     // Dodaje private/readonly do właściwości i promuje je w konstruktorach
+        SetList::NAMED_ARGUMENTS,   // Używa named arguments (np. str_replace(search: ...))
     ]);
 };
